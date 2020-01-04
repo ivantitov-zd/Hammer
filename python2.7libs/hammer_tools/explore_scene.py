d9664909ac6193a9da3497538785b42f525b4889
Titov Ivan <risenforce@mail.ru>
2020-01-05 00:20:59 +0500

    Explore scene alpha
    #117

--- python2.7libs/hammer_tools/explore_scene.py
+++ python2.7libs/hammer_tools/explore_scene.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 
+import json
+
 try:
     from PyQt5.QtWidgets import *
     from PyQt5.QtGui import *
@@ -14,11 +16,15 @@ except ImportError:
 import hou
 
 
+def allSceneItems(root):
+    return root.allSubChildren(recurse_in_locked_nodes=True)
+
+
 class ExploreSceneDialog(QDialog):
     def __init__(self, parent=None):
         super(ExploreSceneDialog, self).__init__(parent, Qt.Window)
 
-        self.setWindowTitle('Hammer: Explore Scene Beta')
+        self.setWindowTitle('Hammer: Explore Scene Alpha')
         self.setStyleSheet(hou.qt.styleSheet())
         self.resize(300, 200)
 
@@ -27,7 +33,7 @@ class ExploreSceneDialog(QDialog):
         main_layout.setSpacing(4)
 
         self.activate_button = QPushButton('Activate Explore Mode')
-        self.activate_button.clicked.connect(self.activateExploreMode)
+        self.activate_button.clicked.connect(lambda: self.activateExploreMode(True))
         main_layout.addWidget(self.activate_button)
 
         self.reset_button = QPushButton('Reset Original Scene State')
@@ -35,17 +41,14 @@ class ExploreSceneDialog(QDialog):
         self.reset_button.clicked.connect(lambda: self.activateExploreMode(False))
         main_layout.addWidget(self.reset_button)
 
-        self.show_all_notes_toggle = QCheckBox('Show All Notes')
-        self.show_all_notes_toggle.setDisabled(True)
-        self.show_all_notes_toggle.clicked.connect(self.applyCurrentSettings)
-        main_layout.addWidget(self.show_all_notes_toggle)
-
         self.show_all_comments_toggle = QCheckBox('Show All Comments')
+        self.show_all_comments_toggle.setChecked(True)
         self.show_all_comments_toggle.setDisabled(True)
         self.show_all_comments_toggle.clicked.connect(self.applyCurrentSettings)
         main_layout.addWidget(self.show_all_comments_toggle)
 
-        self.show_all_text_badges_toggle = QCheckBox('Show All Text Badges')
+        self.show_all_text_badges_toggle = QCheckBox('Show All Descriptions')
+        self.show_all_text_badges_toggle.setChecked(True)
         self.show_all_text_badges_toggle.setDisabled(True)
         self.show_all_text_badges_toggle.clicked.connect(self.applyCurrentSettings)
         main_layout.addWidget(self.show_all_text_badges_toggle)
@@ -59,23 +62,64 @@ class ExploreSceneDialog(QDialog):
         main_layout.addSpacerItem(spacer)
 
     def saveOriginalSettingsToScene(self):
-        pass
+        for item in allSceneItems(hou.root()):
+            if isinstance(item, hou.Node):
+                if not item.parent().isEditable():
+                    continue
+                if 'hammer_explore' in item.userDataDict():
+                    continue
+                data = {}
+                if item.isFlagWritable(hou.nodeFlag.Expose):
+                    data['expose'] = item.isGenericFlagSet(hou.nodeFlag.Expose)
+                if item.isFlagWritable(hou.nodeFlag.Display):
+                    data['display'] = item.isGenericFlagSet(hou.nodeFlag.Display)
+                if item.isFlagWritable(hou.nodeFlag.Template):
+                    data['template'] = item.isGenericFlagSet(hou.nodeFlag.Template)
+                if item.isFlagWritable(hou.nodeFlag.DisplayComment):
+                    data['display_comment'] = item.isGenericFlagSet(hou.nodeFlag.DisplayComment)
+                if item.isFlagWritable(hou.nodeFlag.DisplayDescriptiveName):
+                    data['display_descriptive_name'] = item.isGenericFlagSet(hou.nodeFlag.DisplayDescriptiveName)
+                item.setUserData('hammer_explore', json.dumps(data))
 
-    def loadOriginalSettingsFromScene(self):
-        pass
+    def restoreOriginalSettingsFromScene(self, remove_data=True):
+        for item in allSceneItems(hou.root()):
+            if isinstance(item, hou.Node):
+                if 'hammer_explore' not in item.userDataDict():
+                    continue
+                data = json.loads(item.userData('hammer_explore'))
+                if 'expose' in data:
+                    item.setGenericFlag(hou.nodeFlag.Expose, data.get('expose'))
+                if data.get('display'):
+                    item.setGenericFlag(hou.nodeFlag.Display, True)
+                if 'template' in data:
+                    item.setGenericFlag(hou.nodeFlag.Template, data.get('template'))
+                if 'display_comment' in data:
+                    item.setGenericFlag(hou.nodeFlag.DisplayComment, data.get('display_comment'))
+                if 'display_descriptive_name' in data:
+                    item.setGenericFlag(hou.nodeFlag.DisplayDescriptiveName, data.get('display_descriptive_name'))
+
+                if remove_data:
+                    item.destroyUserData('hammer_explore')
+
+    def applyCurrentSettings(self):
+        for item in allSceneItems(hou.root()):
+            if isinstance(item, hou.Node):
+                if item.isFlagWritable(hou.nodeFlag.DisplayComment):
+                    item.setGenericFlag(hou.nodeFlag.DisplayComment, self.show_all_comments_toggle.isChecked())
+            if item.isFlagWritable(hou.nodeFlag.DisplayDescriptiveName):
+                item.setGenericFlag(hou.nodeFlag.DisplayDescriptiveName, self.show_all_text_badges_toggle.isChecked())
 
     def activateExploreMode(self, enable=True):
         if enable:
             self.saveOriginalSettingsToScene()
+            self.applyCurrentSettings()
+        else:
+            self.restoreOriginalSettingsFromScene()
         self.activate_button.setDisabled(enable)
         self.reset_button.setEnabled(enable)
-        self.show_all_notes_toggle.setEnabled(enable)
         self.show_all_comments_toggle.setEnabled(enable)
         self.show_all_text_badges_toggle.setEnabled(enable)
         self.hide_useless_nodes.setEnabled(enable)
-
-    def applyCurrentSettings(self):
-        pass
 
     @classmethod
     def explore(cls):
