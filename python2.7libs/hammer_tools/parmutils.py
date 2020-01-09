deee1dd408ada117c4710cb70bc04f79239f097f
Titov Ivan <risenforce@mail.ru>
2020-01-10 02:42:13 +0500

    Update parmutils.py

--- python2.7libs/hammer_tools/parmutils.py
+++ python2.7libs/hammer_tools/parmutils.py
@@ -18,13 +18,10 @@ import hou
 
 def setRampParmInterpolation(parm, basis):
     """Sets interpolation for all knots."""
-    try:
-        source_ramp = parm.evalAsRamp()
-        bases = (basis,) * len(source_ramp.basis())
-        new_ramp = hou.Ramp(bases, source_ramp.keys(), source_ramp.values())
-        parm.set(new_ramp)
-    except hou.PermissionError:
-        pass
+    source_ramp = parm.evalAsRamp()
+    bases = (basis,) * len(source_ramp.basis())
+    new_ramp = hou.Ramp(bases, source_ramp.keys(), source_ramp.values())
+    parm.set(new_ramp)
 
 
 def chooseFileAndSetParm(parm):
