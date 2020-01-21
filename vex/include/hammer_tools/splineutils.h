46ebdc695e652cd736c4d3b2992e2eb4763306d9
Titov Ivan <risenforce@mail.ru>
2020-01-21 08:36:07 +0500

    Update splineutils.h

--- vex/include/hammer_tools/splineutils.h
+++ vex/include/hammer_tools/splineutils.h
@@ -3,6 +3,32 @@
 #define _SPLINEUTILS_H_
 
 int
+is_in_range(const int start, end, step)
+{
+    // pass
+}
+
+int
+is_vertex_in_range(const int geometry;
+                   const int vtxnum;
+                   const int start;
+                   const int end;
+                   const int step)
+{
+    // pass
+}
+
+int
+is_point_in_range(const int geometry;
+                  const int ptnum;
+                  const int start;
+                  const int end;
+                  const int step)
+{
+    // pass
+}
+
+int
 is_start_vertex(const int geometry; const int vtxnum)
 {
     int vertex_index = vertexprimindex(geometry, vtxnum);
@@ -37,19 +63,14 @@ int
 is_end_vertex(const int geometry; const int vtxnum)
 {
     int vertex_index = vertexprimindex(geometry, vtxnum);
-    return vertex_index == primvertexcount(geometry, vertexprim(geometry, vtxnum)) - 1;
+    int prim = vertexprim(geometry, vtxnum);
+    return vertex_index == primvertexcount(geometry, prim) - 1;
 }
 
 int
 is_end_point(const int geometry; const int ptnum)
 {
     return is_end_vertex(geometry, pointvertex(geometry, ptnum));
-}
-
-int
-is_end_edge(const int geometry; const int edgenum)
-{
-    // pass
 }
 
 int
@@ -65,6 +86,30 @@ is_end_edge(const int geometry; const int ptnum1, ptnum2)
 }
 
 int
+is_end_edge(const int geometry; const int edgenum)
+{
+    // pass
+}
+
+int
+edge_length(const int geometry; const int vtxnum1, vtxnum2)
+{
+    // pass
+}
+
+int
+edge_length(const int geometry; const int ptnum1, ptnum2)
+{
+    // pass
+}
+
+int
+edge_length(const int geometry; const int edgenum)
+{
+    // pass
+}
+
+int
 is_valid_spline(const int geometry; const int primnum)
 {
     int type = primintrinsic(geometry, 'typeid', primnum);
@@ -75,14 +120,14 @@ is_valid_spline(const int geometry; const int primnum)
 int
 is_knot_vertex(const int geometry; const int vtxnum)
 {
-    int vertex_index = vertexprimindex(geometry, vtxnum);
-    return vertex_index % 3 == 0;
-}
-
-int
-is_control_vertex(const int geometry; const int vtxnum)
-{
-    return !is_knot_vertex(geometry, vtxnum);
+    int prim = vertexprim(geometry, vtxnum);
+    int type = primintrinsic(geometry, 'typeid', prim);
+    if (type == 3)  // Bezier
+    {
+        int vertex_index = vertexprimindex(geometry, vtxnum);
+        return vertex_index % 3 == 0;
+    }
+    return 1;  // Not supported
 }
 
 int
@@ -94,10 +139,13 @@ is_knot_point(const int geometry; const int ptnum)
         return -1;  // Single point
     if (prim_count != 1)
         warning('Geometry has point shared between two or more curves');
-    int type = primintrinsic(geometry, 'typeid', prims[0]);
-    if (type == 3)  // Bezier
-        return is_knot_vertex(geometry, pointvertex(geometry, ptnum));
-    return -1;  // Not supported
+    return is_knot_vertex(geometry, pointvertex(geometry, ptnum));
+}
+
+int
+is_control_vertex(const int geometry; const int vtxnum)
+{
+    return !is_knot_vertex(geometry, vtxnum);
 }
 
 int
@@ -208,6 +256,18 @@ vertex_forward_angle(const int geometry; const int vtxnum)
 
 float
 point_forward_angle(const int geometry; const int ptnum)
+{
+    // pass
+}
+
+float
+vertex_backward_angle(const int geometry; const int vtxnum)
+{
+    // pass
+}
+
+float
+point_backward_angle(const int geometry; const int ptnum)
 {
     // pass
 }
