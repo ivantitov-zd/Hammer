be5148feef692232e5e52ac41e78038b84a91f23
Titov Ivan <risenforce@mail.ru>
2020-01-21 07:30:18 +0500

    Update splineutils.h

--- vex/include/hammer_tools/splineutils.h
+++ vex/include/hammer_tools/splineutils.h
@@ -3,13 +3,54 @@
 #define _SPLINEUTILS_H_
 
 int
-is_control_vertex(const int geometry; const int vtxnum)
+is_start_vertex(const int geometry; const int vtxnum)
 {
-    // pass
+    int vertex_index = vertexprimindex(geometry, vtxnum);
+    return vertex_index == 0;
 }
 
 int
-is_control_point(const int geometry; const int ptnum)
+is_start_point(const int geometry; const int ptnum)
+{
+    return is_start_vertex(geometry, pointvertex(geometry, ptnum));
+}
+
+int
+is_end_vertex(const int geometry; const int vtxnum)
+{
+    int vertex_index = vertexprimindex(geometry, vtxnum);
+    return vertex_index == primvertexcount(geometry, vertexprim(geometry, vtxnum)) - 1;
+}
+
+int
+is_end_point(const int geometry; const int ptnum)
+{
+    return is_end_vertex(geometry, pointvertex(geometry, ptnum));
+}
+
+int
+is_valid_spline(const int geometry; const int primnum)
+{
+    int type = primintrinsic(geometry, 'typeid', primnum);
+
+    return (primvertexcount(geometry, primnum) - 4) % 3 == 0;
+}
+
+int
+is_knot_vertex(const int geometry; const int vtxnum)
+{
+    int vertex_index = vertexprimindex(geometry, vtxnum);
+    return vertex_index % 3 == 0;
+}
+
+int
+is_control_vertex(const int geometry; const int vtxnum)
+{
+    return !is_knot_vertex(geometry, vtxnum);
+}
+
+int
+is_knot_point(const int geometry; const int ptnum)
 {
     int prims[] = pointprims(geometry, ptnum);
     int prim_count = len(prims);
@@ -17,23 +58,66 @@ is_control_point(const int geometry; const int ptnum)
         return -1;  // Single point
     if (prim_count != 1)
         warning('Geometry has point shared between two or more curves');
-    int prim = prims[0];
-    int type = primintrinsic(geometry, 'typeid', prim);
-    if (type == 1 && primintrinsic(geometry, 'closed', prim) == 0)  // Polyline
-    {
-        return 1;
-    } else if (type == 2)  // NURBS
-    {
-        return 1;
-    } else if (type == 3)  // Bezier
-    {
-        int vertex_index = vertexprimindex(geometry, pointvertex(geometry, ptnum));
-        return vertex_index % 3 == 0;
-    }
+    int type = primintrinsic(geometry, 'typeid', prims[0]);
+    if (type == 3)  // Bezier
+        return is_knot_vertex(geometry, pointvertex(geometry, ptnum));
     return -1;  // Not curve
 }
 
-int prev_control_vertex(const int geometry; const int vtxnum)
+int
+is_control_point(const int geometry; const int ptnum)
+{
+    return !is_knot_point(geometry, ptnum);
+}
+
+int
+prev_knot_vertex(const int geometry; const int vtxnum)
+{
+    // pass
+}
+
+int
+prev_knot_point(const int geometry; const int ptnum)
+{
+    // pass
+}
+
+int
+next_knot_vertex(const int geometry; const int vtxnum)
+{
+    // pass
+}
+
+int
+next_knot_point(const int geometry; const int ptnum)
+{
+    // pass
+}
+
+int
+knot_vertex(const int geometry; const int vtxnum)
+{
+    int vertex_index = vertexprimindex(geometry, vtxnum);
+    return (int)rint(ceil((vertex_index - 1) / 3.0) * 3);
+}
+
+int
+knot_point(const int geometry; const int ptnum)
+{
+    int prims[] = pointprims(geometry, ptnum);
+    int prim_count = len(prims);
+    if (prim_count == 0)
+        return -1;  // Single point
+    if (prim_count != 1)
+        warning('Geometry has point shared between two or more curves');
+    int type = primintrinsic(geometry, 'typeid', prims[0]);
+    if (type == 3)  // Bezier
+        return knot_vertex(geometry, pointvertex(geometry, ptnum));
+    return -1;  // Not curve
+}
+
+int
+prev_control_vertex(const int geometry; const int vtxnum)
 {
     // pass
 }
@@ -45,40 +129,15 @@ prev_control_point(const int geometry; const int ptnum)
 }
 
 int
+next_control_vertex(const int geometry; const int vtxnum)
+{
+    // pass
+}
+
+int
 next_control_point(const int geometry; const int ptnum)
 {
     // pass
-}
-
-int
-control_vertex(const int geometry; const int vtxnum)
-{
-    // pass
-}
-
-int
-control_point(const int geometry; const int ptnum)
-{
-    int prims[] = pointprims(geometry, ptnum);
-    int prim_count = len(prims);
-    if (prim_count == 0)
-        return -1;  // Single point
-    if (prim_count != 1)
-        warning('Geometry has point shared between two or more curves');
-    int prim = prims[0];
-    int type = primintrinsic(geometry, 'typeid', prim);
-    if (type == 1 && primintrinsic(geometry, 'closed', prim) == 0)  // Polyline
-    {
-        return ptnum;
-    } else if (type == 2)  // NURBS
-    {
-        return ptnum;
-    } else if (type == 3)  // Bezier
-    {
-        int vertex_index = vertexprimindex(geometry, pointvertex(geometry, ptnum));
-        return (int)rint(ceil((vertex_index - 1) / 3.0) * 3);
-    }
-    return -1;  // Not curve
 }
 
 #endif  // _SPLINEUTILS_H_
