dc547207885830b6df287f1b75c3f047a6c9d34a
Titov Ivan <risenforce@mail.ru>
2020-02-11 14:34:29 +0500

    transfer

--- python2.7libs/hammer_tools/parmutils.py
+++ python2.7libs/hammer_tools/parmutils.py
@@ -93,30 +93,3 @@ def toggleParmExpression(parm):
 
 toggleExpression = toggleParmExpression  # Todo: refactor in xmls
 
-
-def playChopAudio(node):
-    if settings.value('hammer.audio.play.use_external_player'):
-        if node.type().name() == 'chopnet':
-            nodes = node.glob('*')
-            for node in nodes:
-                if node.isAudioFlagSet():
-                    break
-        if node.type().name() == 'file':
-            path = node.parm('file').eval()
-            if os.path.exists(path) and path.endswith(('.wav', '.mp3')):
-                os.startfile(path)
-        else:
-            _, path = tempfile.mkstemp('.wav', node.name() + '_', hou.getenv('TEMP'))
-            node.saveClip(path)
-            os.startfile(path)
-    else:
-        hou.audio.useTestMode()
-        hou.audio.useChops()
-        hou.audio.setChopPath(node.path())
-        hou.audio.play()
-
-
-def setSceneChopAudio(node):
-    hou.audio.useTimeLineMode()
-    hou.audio.useChops()
-    hou.audio.setChopPath(node.path())
