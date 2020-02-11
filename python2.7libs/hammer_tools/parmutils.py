a4755604258d646685458797dc1eed62c04d14cb
Titov Ivan <risenforce@mail.ru>
2020-02-11 14:15:58 +0500

    Audio Settings

--- python2.7libs/hammer_tools/parmutils.py
+++ python2.7libs/hammer_tools/parmutils.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 
 import json
 import os
+import tempfile
 
 try:
     from PyQt5.QtWidgets import *
@@ -17,6 +18,9 @@ except ImportError:
 import hou
 
 from .utils import openLocation
+from .settings import SettingsManager
+
+settings = SettingsManager.instance()
 
 
 def setRampParmInterpolation(parm, basis):
@@ -88,3 +92,31 @@ def toggleParmExpression(parm):
 
 
 toggleExpression = toggleParmExpression  # Todo: refactor in xmls
+
+
+def playChopAudio(node):
+    if settings.value('hammer.play_sound.use_external_player'):
+        if node.type().name() == 'chopnet':
+            nodes = node.glob('*')
+            for node in nodes:
+                if node.isAudioFlagSet():
+                    break
+        if node.type().name() == 'file':
+            path = node.parm('file').eval()
+            if os.path.exists(path) and path.endswith(('.wav', '.mp3')):
+                os.startfile(path)
+        else:
+            _, path = tempfile.mkstemp('.wav', node.name() + '_', hou.getenv('TEMP'))
+            node.saveClip(path)
+            os.startfile(path)
+    else:
+        hou.audio.useTestMode()
+        hou.audio.useChops()
+        hou.audio.setChopPath(node.path())
+        hou.audio.play()
+
+
+def setSceneChopAudio(node):
+    hou.audio.useTimeLineMode()
+    hou.audio.useChops()
+    hou.audio.setChopPath(node.path())
