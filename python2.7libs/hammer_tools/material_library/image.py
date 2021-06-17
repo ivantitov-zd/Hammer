6a41457ce5b556a5ea3c47bad714b40161502530
Ivan Titov <risenforce@mail.ru>
2021-06-17 08:04:00 +0300

    Material Library
    Added universal function for image loading

--- python2.7libs/hammer_tools/material_library/image.py
+++ python2.7libs/hammer_tools/material_library/image.py
@@ -1,7 +1,13 @@
+import os
+import subprocess
+import tempfile
+
 try:
-    from PyQt5.QtCore import QBuffer, QIODevice
+    from PyQt5.QtCore import QBuffer, QIODevice, Qt
+    from PyQt5.QtGui import QImage
 except ImportError:
-    from PySide2.QtCore import QBuffer, QIODevice
+    from PySide2.QtCore import QBuffer, QIODevice, Qt
+    from PySide2.QtGui import QImage
 
 
 def imageToBytes(image):
@@ -11,3 +17,17 @@ def imageToBytes(image):
     data = buffer.data()
     buffer.close()
     return data
+
+
+def loadImage(path):
+    _, ext = os.path.splitext(path)
+    if ext in {'png', 'bmp', 'tga', 'tif', 'tiff', 'jpg', 'jpeg'}:
+        return QImage(path)
+
+    temp_path = os.path.join(tempfile.gettempdir(), str(os.getpid()) + 'hammer_temp_image.png')
+    temp_path = temp_path.replace('\\', '/')
+    subprocess.call('iconvert -g off {0} {1}'.format(path, temp_path))
+    try:
+        return QImage(temp_path)
+    finally:
+        os.remove(temp_path)
